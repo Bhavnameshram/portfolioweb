0f341272b03557cf7c360c80f778c23c5d4388a8
Bhavnameshram <bhawanameshram1910@gmail.com>
2025-11-18 12:55:42 +0530

    changes1 navbar

---
+++ portfolioweb/src/compnents/Navbars.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function navbars() {
+  return (
+    <div>
+      <div class="bg">
+        <div class="container">
+            <nav>
+                <img src="./img/I_am__B_-removebg-preview (1).png" class="logo" alt="">
+                <ul>
+                    <li><a href="#"></a>Home</li>
+                    <li><a href="#"></a>About</li>
+                    <li><a href="#"></a>Services</li>
+                    <li><a href="#"></a>Portfolio</li>
+                    <li><a href="#"></a>Contact</li>
+                </ul>
+            </nav>
+            <div class="data">
+                <p>Fronted Devloper</p>
+                <h1>Hi, I'm <br> <span>Bhavna Meshram</span> <br>From Nagpur</h1>
+            </div>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default navbars
